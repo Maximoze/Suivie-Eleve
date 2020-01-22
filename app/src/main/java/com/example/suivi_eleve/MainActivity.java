f0f93dee98db3c5ce99e86e5a70d16535625c3b3
abdlynng <abdlynng@gmail.com>
2020-01-22 21:31:15 +0100

    ajout Intent Activity sur le bouton Enseignant

--- app/src/main/java/com/example/suivi_eleve/MainActivity.java
+++ app/src/main/java/com/example/suivi_eleve/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.suivi_eleve;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Enseignant button */
+    public void toEnseingant (View view) {
+        // Do something in response to button
+        Intent intentEnseignant = new Intent(this, EnseignantDashbordActivity.class);
+
+        startActivity(intentEnseignant);
+    }
+
 }
