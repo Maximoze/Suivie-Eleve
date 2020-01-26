ca6f6e195e676c624770a8cc9f0311593863c9c8
Maximoze <maguette357@gmail.com>
2020-01-26 19:29:07 +0000

    Ajout du design pour la partie activites a faire  et des clicks listerner sur les bouttons de la drawer navigation

---
+++ app/src/main/java/com/example/suivi_eleve/ProfilParentActivity.java
@@ -0,0 +1,19 @@
+package com.example.suivi_eleve;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class ProfilParentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_parent);
+
+
+    }
+}
