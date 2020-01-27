1615b2245092cd3d4d426d50919608b11ee5245e
Maximoze <maguette357@gmail.com>
2020-01-27 09:46:49 +0000

    bug on latest update resolved

--- app/src/main/java/com/example/suivi_eleve/ui/Activites/ActivitesFragment.java
+++ app/src/main/java/com/example/suivi_eleve/ui/Activites/ActivitesFragment.java
@@ -1,6 +1,7 @@
 package com.example.suivi_eleve.ui.Activites;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,13 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.suivi_eleve.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import static android.content.ContentValues.TAG;
 
 public class ActivitesFragment extends Fragment {
 
@@ -24,12 +32,16 @@ public class ActivitesFragment extends Fragment {
                 ViewModelProviders.of(this).get(ActivitesViewModel.class);
         View root = inflater.inflate(R.layout.fragment_activites, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
+
+        final TextView libellecontent = root.findViewById(R.id.libelecontent);
+
         /*activitesViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
         });*/
+
         return root;
     }
 }
