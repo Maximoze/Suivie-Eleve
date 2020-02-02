1680683c3e0d108aeaeb01cf8424fcc8daba9112
kaya1995 <aissatou.faye2@uadb.edu.sn>
2020-02-02 18:58:23 +0000

    partie observation debut

--- app/src/main/java/com/example/suivi_eleve/ObservationActivity.java
+++ app/src/main/java/com/example/suivi_eleve/ObservationActivity.java
@@ -3,12 +3,32 @@ package com.example.suivi_eleve;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ObservationActivity extends AppCompatActivity {
+
+    TextView eleve;
+    TextView type0bservation;
+    TextView note;
+    EditText contenue_eleve;
+    EditText contenue_type;
+    EditText contenue_note;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_observation);
+
+        eleve = findViewById(R.id.eleve);
+        type0bservation = findViewById(R.id.typeO);
+        note = findViewById(R.id.note);
+
+        contenue_eleve = findViewById(R.id.contenu_eleve);
+        contenue_type = findViewById(R.id.contenu_type);
+        contenue_note = findViewById(R.id.contenu_note);
+
     }
 }
